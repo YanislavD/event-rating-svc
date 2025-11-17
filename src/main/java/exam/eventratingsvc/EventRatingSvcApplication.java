7c985c1418cd257205eaecf1437e09552b55e348
YanislavD <jnislav.dragiiski02@gmail.com>
2025-11-17 19:12:41 +0100

    add all

--- src/main/java/exam/eventratingsvc/EventRatingSvcApplication.java
+++ src/main/java/exam/eventratingsvc/EventRatingSvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventRatingSvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventRatingSvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventRatingSvcApplication.class, args);
+    }
 
 }
